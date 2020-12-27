08414a60269fc45e5e647155aab9620804c58616
Nhienvtq <nhien.vtq@gmail.com>
2020-12-28 03:47:41 +0700

    Change Record viewmodel interface

--- app/src/main/java/com/example/trackmeex/RecordViewModel.kt
+++ app/src/main/java/com/example/trackmeex/RecordViewModel.kt
@@ -1,35 +1,168 @@
 package com.example.trackmeex
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.location.Location
 import android.util.Log
+import android.widget.Chronometer
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.*
+import com.google.maps.android.SphericalUtil
 
 
-class RecordViewModel: ViewModel()
+class RecordViewModel: ViewModel(), OnMapReadyCallback
 {
 
+    val _fusedLocationClient: MutableLiveData<FusedLocationProviderClient> by lazy{ MutableLiveData<FusedLocationProviderClient>()}
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
     var sizespeedList: Int = 0
+    var _Bitmapstring: String? = null
+    var _distance: Float = 0f
+    var _speed_avg: Float = 0f
+    var _past_timerpoint: Float = 0f
+    var _current_timerpoint: Float = 0f
 
-
+    val _timer: MutableLiveData<Chronometer> by lazy{ MutableLiveData<Chronometer>()}
     val _distance_total: MutableLiveData<Float> by lazy{ MutableLiveData<Float>()}
-    val _distance: MutableLiveData<Float> by lazy{ MutableLiveData<Float>()}
-    val _speed_avg: MutableLiveData<Float> by lazy{ MutableLiveData<Float>()}
     val _speed: MutableLiveData<Float> by lazy{ MutableLiveData<Float>()}
-    val _past_timerpoint: MutableLiveData<Float> by lazy{ MutableLiveData<Float>()}
-    val _current_timerpoint: MutableLiveData<Float> by lazy{ MutableLiveData<Float>()}
     val _mapView: MutableLiveData<MapView> by lazy{MutableLiveData<MapView>()}
     val _map: MutableLiveData<GoogleMap> by lazy{MutableLiveData<GoogleMap>()}
     val _past_location: MutableLiveData<Location> by lazy{MutableLiveData<Location>()}
     val _current_location: MutableLiveData<Location> by lazy{MutableLiveData<Location>()}
     val _marker: MutableLiveData<Marker> by lazy{MutableLiveData<Marker>()}
+    val _markerList: ArrayList<Marker> = ArrayList()
     val _stateOnRecord: MutableLiveData<Boolean> by lazy{MutableLiveData<Boolean>()}
 
-    override fun onCleared() {
-        super.onCleared()
-        Log.i("flowTag", "RecordViewModel Cleared")
+    init {
+        _past_location.value = null
+        _current_location.value = null
+        _current_timerpoint = 0f
+        _past_timerpoint = 0f
+        _stateOnRecord.value = true
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        _map.value = googleMap
+        _distance_total.value = 0f
+        _speed.value = 0f
+        _speed_avg = 0f
+        _distance = 0f
+
+        //set default showing position
+        val current_latLng = LatLng(37.4149, -122.1490)
+        _map.value?.animateCamera(CameraUpdateFactory.newLatLngZoom(current_latLng, 10f))
+        getLocationUpdates()
+        startLocationUpdates()
+    }
+    private fun getLocationUpdates() {
+        locationRequest = LocationRequest()
+        //update each 3s
+        locationRequest.interval = 3000 //mili sec
+        locationRequest.fastestInterval = 2000 //mili sec
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        //call back for location update
+        locationCallback = object : LocationCallback() {
+            @SuppressLint("SetTextI18n")
+            override fun onLocationResult(locationResult: LocationResult) {
+                //get valid location
+                if (locationResult.locations.isNotEmpty() && _stateOnRecord.value == true) {
+                    _current_location.value = locationResult.lastLocation
+                    val current_latLng = LatLng(_current_location.value!!.latitude, _current_location.value!!.longitude)
+                    val markerOptions = MarkerOptions().position(current_latLng)
+                    if (_current_location.value != null) {
+                        //init first location
+                        if (_past_location.value == null) {
+                            _past_location.value = _current_location.value
+                            _map.value?.moveCamera(CameraUpdateFactory.newLatLngZoom(current_latLng,15f))
+                            _marker.value = _map.value?.addMarker(markerOptions.title("You").icon(
+                                    BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)))
+                        }
+                        val past_latLng = LatLng( _past_location.value!!.latitude,  _past_location.value!!.longitude)
+                        //distance - time - speed processing
+                        _distance = "%.2f".format((SphericalUtil.computeDistanceBetween(past_latLng,current_latLng)/1000)).toFloat()
+                        val time_interval = "%.2f".format(_current_timerpoint - _past_timerpoint).toFloat()
+                        _speed.value = "%.2f".format(_distance/(time_interval/3600)).toFloat()
+                        if ((past_latLng != current_latLng)){
+                            sizespeedList += 1
+                            //hide former markers but start point
+                            if (_past_timerpoint != 0f) _marker.value?.isVisible = false
+                            //add marker
+                            _marker.value = _map.value?.addMarker(markerOptions!!.title("You").icon(BitmapDescriptorFactory.defaultMarker(
+                                BitmapDescriptorFactory.HUE_RED)))
+                            _markerList.add(_marker.value!!)
+                            //draw path
+                            val line = _map.value!!.addPolyline(PolylineOptions()
+                                .add(past_latLng ,current_latLng ).width(5f).color(Color.RED))
+                            _past_location.value = _current_location.value
+                            _past_timerpoint = _current_timerpoint
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        _fusedLocationClient.value?.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null
+        )
+    }
+
+    fun zoomfullCourse(){
+        //zoom to view full course
+        val builder = LatLngBounds.Builder()
+        for (marker in _markerList) {
+            builder.include(marker.position)
+            val bounds = builder.build()
+            val padding = 200
+            _map.value?.setMaxZoomPreference(15f)
+            _map.value?.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, padding))
+        }
+    }
+
+    fun formattimeDisplay(seconds: Float):String{
+        val hour: Int = (seconds/3600).toInt()
+        val minute: Int = ((seconds - hour*3600)/60).toInt()
+        val second: Int = ((seconds - hour*3600)- minute*60).toInt()
+
+        val hourHH: String
+        if (hour < 10){
+            hourHH = "0" + hour.toString()
+        } else hourHH = hour.toString()
+        val minuteMM: String
+        if (minute < 10){
+            minuteMM = "0" + minute.toString()
+        } else minuteMM = minute.toString()
+        val secondSS: String
+        if (second < 10){
+            secondSS = "0" + second.toString()
+        } else secondSS = second.toString()
+        val timeHHMMSS: String = hourHH + ":" + minuteMM + ":" + secondSS
+        return timeHHMMSS
+    }
+
+    var _distanceDB: String? = null
+    var _timeDB: String? = null
+    var _speedDB: String? = null
+    fun getinsertDataToDatabase() {
+        //prepare room data
+        _distanceDB = (_distance_total.value).toString() + " km"
+        _timeDB = formattimeDisplay(_current_timerpoint)
+        if (sizespeedList != 0){
+            _speedDB = (_speed_avg.div(sizespeedList)).toString() + " km/h"
+        } else {
+            _speedDB = "0.00 km/h"
+        }
     }
 }
--- app/src/main/java/com/example/trackmeex/recordFragment.kt
+++ app/src/main/java/com/example/trackmeex/recordFragment.kt
@@ -2,11 +2,9 @@ package com.example.trackmeex
 
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
-import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
 import android.os.SystemClock
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -22,44 +20,28 @@ import com.example.trackmeex.data.Section
 import com.example.trackmeex.data.SectionViewModel
 import com.example.trackmeex.databinding.FragmentRecordBinding
 import com.google.android.gms.location.*
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.*
-import com.google.maps.android.SphericalUtil
 
 
-class recordFragment : Fragment(), OnMapReadyCallback
+class recordFragment : Fragment()
 {
     private lateinit var binding: FragmentRecordBinding
     private lateinit var viewModel: RecordViewModel
     private lateinit var hisSectionViewModel: SectionViewModel
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var locationRequest: LocationRequest
-    private lateinit var locationCallback: LocationCallback
-
-    private lateinit var savesnapshot: Bitmap
-    private var Bitmapstring: String? = null
     private var mapView: MapView? = null
     private var map: GoogleMap? = null
-    private var marker: Marker? = null
-    private val markerList: ArrayList<Marker> = ArrayList()
     private var past_location: Location? = null
-    private var past_timerpoint: Float? = null
     private var current_location: Location? = null
-    private var current_timerpoint: Float? = null
-
-    private var distance_total: Float? = null
-    private var distance: Float? = null
 
     private lateinit var timer: Chronometer
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Binding inflate the layout for this fragment
         binding = DataBindingUtil.inflate(layoutInflater,R.layout.fragment_record, container, false)
 
@@ -68,28 +50,46 @@ class recordFragment : Fragment(), OnMapReadyCallback
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         activity?.getWindow()?.attributes?.flags = WindowManager.LayoutParams.FLAG_FULLSCREEN
-        //set up duration timer
-        timer = binding.chronometer
-        current_timerpoint = 0f
-        timer.setOnChronometerTickListener {
-            viewModel._current_timerpoint.value = ((SystemClock.elapsedRealtime() - timer.base).toFloat())/1000
-            binding.timdDisplaytextView.text = formattimeDisplay(current_timerpoint!!)
-        }
-        //Create history viewmodel - Recyclerview
-        viewModelmanage()
-        //process viewButton action
+
         buttonsClickmanage()
-        //binding and prepare mapview
+        viewModelmanage()
         viewModel._mapView.value = binding.mapView
         viewModel._mapView.value!!.onCreate(savedInstanceState)
-        viewModel._mapView.value!!.getMapAsync(this)
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        viewModel._mapView.value!!.getMapAsync(viewModel)
+        viewModel._fusedLocationClient.value = LocationServices.getFusedLocationProviderClient(requireContext())
+
+        timer = binding.chronometer
+        timer.setOnChronometerTickListener {
+            viewModel._current_timerpoint = ((SystemClock.elapsedRealtime() - timer.base).toFloat())/1000
+            binding.timdDisplaytextView.text = viewModel.formattimeDisplay(viewModel._current_timerpoint)
+        }
+    }
+    private fun buttonsClickmanage(){
+        binding.stopimageView.setOnClickListener {
+            //prepare data
+            viewModel.getinsertDataToDatabase()
+            val section = Section(0, viewModel._distanceDB!!, viewModel._speedDB!!,  viewModel._timeDB!!,  viewModel._Bitmapstring!!)
+            //save data in room
+            hisSectionViewModel.addSection(section)
+            Toast.makeText(requireContext(), "Successfully add to database", Toast.LENGTH_SHORT).show()
+            //navigate back to history
+            findNavController().navigate(R.id.action_recordFragment_to_historyFragment)
+        }
+        binding.pauseimageView.setOnClickListener{
+            viewModel._stateOnRecord.value = false
+        }
+        binding.replayimageView.setOnClickListener{
+            viewModel._stateOnRecord.value = true
+        }
     }
     @SuppressLint("SetTextI18n")
     private fun viewModelmanage(){
         hisSectionViewModel = ViewModelProvider(this).get(SectionViewModel::class.java)
 
         viewModel = ViewModelProvider(this).get(RecordViewModel::class.java)
+        viewModel._fusedLocationClient.observe(viewLifecycleOwner, Observer {
+            fusedLocationClient = viewModel._fusedLocationClient.value!!
+        })
 
         viewModel._mapView.observe(viewLifecycleOwner, Observer {
             mapView = viewModel._mapView.value
@@ -103,26 +103,21 @@ class recordFragment : Fragment(), OnMapReadyCallback
         viewModel._current_location.observe(viewLifecycleOwner, Observer {
             current_location = viewModel._current_location.value
         })
-        viewModel._past_timerpoint.observe(viewLifecycleOwner, Observer {
-            past_timerpoint = viewModel._past_timerpoint.value
-        })
-        viewModel._current_timerpoint.observe(viewLifecycleOwner, Observer {
-            current_timerpoint = viewModel._current_timerpoint.value
+        viewModel._timer.observe(viewLifecycleOwner, Observer {
+            timer = viewModel._timer.value!!
         })
         viewModel._marker.observe(viewLifecycleOwner, Observer {
-            marker = viewModel._marker.value
-        })
-        viewModel._distance.observe(viewLifecycleOwner, Observer {
-            distance = viewModel._distance.value!!
+            viewModel._markerList.add(viewModel._marker.value!!)
+            viewModel.zoomfullCourse()
+            viewModel._distance_total.value = viewModel._distance_total.value?.plus(
+                viewModel._distance)
+            viewModel._speed_avg = viewModel._speed_avg.plus(viewModel._speed.value!!)
         })
         viewModel._distance_total.observe(viewLifecycleOwner, Observer {
-            distance_total = viewModel._distance_total.value!!
-            binding.distancttextView.text = ("%.2f".format(it)) + " km"
+            binding.distancttextView.text = "$it km"
         })
         viewModel._speed.observe(viewLifecycleOwner, Observer {
-            binding.speedtextView.text = "%.2f".format(viewModel._speed.value) + " km/h"
-        })
-        viewModel._speed_avg.observe(viewLifecycleOwner, Observer {
+            binding.speedtextView.text = "$it km/h"
         })
         viewModel._stateOnRecord.observe(viewLifecycleOwner, Observer {
             if (viewModel._stateOnRecord.value == true){
@@ -130,9 +125,17 @@ class recordFragment : Fragment(), OnMapReadyCallback
                 binding.replayimageView.visibility = View.INVISIBLE
                 binding.stopimageView.visibility = View.INVISIBLE
                 viewModel._marker.value?.isVisible = false
-                timer.base = SystemClock.elapsedRealtime() - 1000*(current_timerpoint!!).toLong()
+                viewModel._timer.value?.base = SystemClock.elapsedRealtime() - 1000*(viewModel._current_timerpoint).toLong()
                 timer.start()
             } else {
+                //take snapshot - might miss
+                map?.snapshot(object : GoogleMap.SnapshotReadyCallback {
+                    override fun onSnapshotReady(snapshot: Bitmap?) {
+                        if (snapshot != null) {
+                            viewModel._Bitmapstring = ImageBitmapString.BitMapToString(snapshot)
+                        }
+                    }
+                })
                 binding.pauseimageView.visibility = View.INVISIBLE
                 binding.replayimageView.visibility = View.VISIBLE
                 binding.stopimageView.visibility = View.VISIBLE
@@ -140,156 +143,6 @@ class recordFragment : Fragment(), OnMapReadyCallback
                 timer.stop()
             }
         })
-        viewModel._stateOnRecord.value = true
-    }
-    private fun buttonsClickmanage(){
-        binding.stopimageView.setOnClickListener {
-            //save data
-            insertDataToDatabase()
-            //navigate back to history
-            findNavController().navigate(R.id.action_recordFragment_to_historyFragment)
-        }
-        binding.pauseimageView.setOnClickListener{
-            viewModel._stateOnRecord.value = false
-            //zoom to view full course
-            zoomfullCourse()
-            //take snapshot of full course
-            map?.snapshot(object : GoogleMap.SnapshotReadyCallback {
-                override fun onSnapshotReady(snapshot: Bitmap?) {
-                    if (snapshot != null) {
-                        savesnapshot = snapshot
-                        Bitmapstring = ImageBitmapString.BitMapToString(savesnapshot)
-                    }
-                }
-            })
-        }
-        binding.replayimageView.setOnClickListener{
-            viewModel._stateOnRecord.value = true
-        }
-    }
-    private fun formattimeDisplay(seconds: Float):String{
-        val hour: Int = (seconds/3600).toInt()
-        val minute: Int = ((seconds - hour*3600)/60).toInt()
-        val second: Int = ((seconds - hour*3600)- minute*60).toInt()
-
-        val hourHH: String
-        if (hour < 10){
-            hourHH = "0" + hour.toString()
-        } else hourHH = hour.toString()
-        val minuteMM: String
-        if (minute < 10){
-            minuteMM = "0" + minute.toString()
-        } else minuteMM = minute.toString()
-        val secondSS: String
-        if (second < 10){
-            secondSS = "0" + second.toString()
-        } else secondSS = second.toString()
-        val timeHHMMSS: String = hourHH + ":" + minuteMM + ":" + secondSS
-        return timeHHMMSS
-    }
-    private fun insertDataToDatabase() {
-        //prepare room data
-        val distance = "%.2f".format(viewModel._distance_total.value) + " km"
-        val time = formattimeDisplay(current_timerpoint!!)
-        val speed: String
-        if (viewModel.sizespeedList != 0){
-            speed = "%.2f".format(viewModel._speed_avg.value?.toFloat()
-                ?.div(viewModel.sizespeedList)) + " km/h"
-        } else {
-            speed = "0.00 km/h"
-        }
-
-        //save data in room
-        val section = Section(0, distance, speed, time, Bitmapstring!!)
-        hisSectionViewModel.addSection(section)
-        Toast.makeText(requireContext(), "Successfully add to database", Toast.LENGTH_SHORT).show()
-    }
-    override fun onMapReady(googleMap: GoogleMap?) {
-        viewModel._map.value = googleMap
-        viewModel._distance_total.value = 0f
-        viewModel._speed.value = 0f
-        viewModel._speed_avg.value = 0f
-        viewModel._distance.value = 0f
-        getLocationAccess()
-    }
-    private fun getLocationAccess() {
-            getLocationUpdates()
-            startLocationUpdates()
-    }
-    private fun getLocationUpdates() {
-        locationRequest = LocationRequest()
-        locationRequest.interval = 3000
-        locationRequest.fastestInterval = 2000
-        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        //call back for location update
-        locationCallback = object : LocationCallback() {
-            @SuppressLint("SetTextI18n")
-            override fun onLocationResult(locationResult: LocationResult) {
-                //get valid location
-                if (locationResult.locations.isNotEmpty() && viewModel._stateOnRecord.value == true) {
-                    viewModel._current_location.value = locationResult.lastLocation
-                    val current_latLng = LatLng(viewModel._current_location.value!!.latitude, viewModel._current_location.value!!.longitude)
-                    val markerOptions = MarkerOptions().position(current_latLng)
-                    if (current_location!= null) {
-                        //init first location
-                        if (past_location == null){
-                            viewModel._past_location.value = viewModel._current_location.value
-                            past_timerpoint = 0f
-                            viewModel._map.value?.moveCamera(CameraUpdateFactory.newLatLngZoom(current_latLng, 15f))
-                            viewModel._marker.value =viewModel._map.value?.addMarker(markerOptions.title("You").icon(BitmapDescriptorFactory.defaultMarker(
-                                BitmapDescriptorFactory.HUE_BLUE)))
-                            markerList.add(viewModel._marker.value!!)
-                        }
-                        val past_latLng = LatLng( viewModel._past_location.value!!.latitude,  viewModel._past_location.value!!.longitude)
-                        //Process distance and speed
-                        viewModel._distance.value = (SphericalUtil.computeDistanceBetween(past_latLng,current_latLng)/1000).toFloat()
-                        val time_interval = current_timerpoint!! - past_timerpoint!!
-                        viewModel._speed.value = (viewModel._distance.value!!/(time_interval/3600))
-                        //detect change in location -> update
-                        if ((past_latLng != current_latLng)){
-                            viewModel._distance_total.value = viewModel._distance_total.value?.plus(
-                                viewModel._distance.value!!)
-                            viewModel._speed_avg.value = viewModel._speed_avg.value?.plus(viewModel._speed.value!!)
-                            viewModel.sizespeedList += 1
-                            if (past_timerpoint != 0f){ //implement onpause for hide marker on replay
-                                viewModel._marker.value?.isVisible = false
-                            }
-                            viewModel._marker.value =viewModel._map.value?.addMarker(markerOptions!!.title("You").icon(BitmapDescriptorFactory.defaultMarker(
-                                BitmapDescriptorFactory.HUE_RED)))
-                            markerList.add(viewModel._marker.value!!)
-                            zoomfullCourse()
-                            val line = viewModel._map.value!!.addPolyline(PolylineOptions()
-                                    .add(past_latLng ,current_latLng ).width(5f).color(Color.RED))
-                            viewModel._past_location.value = viewModel._current_location.value
-                            past_timerpoint = current_timerpoint
-                        }
-                    }
-                }
-            }
-        }
-    }
-    private fun zoomfullCourse(){
-        //zoom to view full course
-        val builder = LatLngBounds.Builder()
-        for (marker in markerList) {
-            builder.include(marker.position)
-            val bounds = builder.build()
-            val padding = 200
-            viewModel._map.value?.setMaxZoomPreference(18f)
-            viewModel._map.value?.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, padding))
-        }
-    }
-    @SuppressLint("MissingPermission")
-    private fun startLocationUpdates() {
-        fusedLocationClient.requestLocationUpdates(
-            locationRequest,
-            locationCallback,
-            null
-        )
-    }
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView?.onSaveInstanceState(outState)
     }
     override fun onResume() {
         mapView!!.onResume()
